9ce2f9bb615f297c524c788fbd67b00997ce7f32
HANSEOHYUN <000312@daum.net>
2025-08-09 01:37:25 +0900

    Initial commit

---
+++ src/main/java/com/example/sakak/SakakApplication.java
@@ -0,0 +1,13 @@
+package com.example.sakak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sakak/SakakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sakak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
